75e26b39526e3f8c563b8aa215816f160fea3097
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-02-16 21:40:03 +0100

    Hackiest player and pathfinding evur

--- OnTheRun/Assets/Code/Player.cs
+++ OnTheRun/Assets/Code/Player.cs
@@ -3,13 +3,119 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject CDown;
+    public GameObject CUp;
+    public GameObject CLeft;
+    public GameObject CRight;
+
+    public float Speed = 5f;
+
+    enum EDirection {
+        Left = 1,
+        Right = 2,
+        Up = 4,
+        Down = 8,
+        None = 16
+    }
+
+    private EDirection currentDirection = EDirection.None;
+    //private EDirection previousDirection = EDirection.None;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ( Input.GetKeyDown( KeyCode.W ) || Input.GetKeyDown( KeyCode.UpArrow ) ) {
+            ChangeDirection( EDirection.Up );
+            SwitchCharacter();
+        }
+
+        if ( Input.GetKeyDown( KeyCode.S ) || Input.GetKeyDown( KeyCode.DownArrow ) ) {
+            ChangeDirection( EDirection.Down );
+            SwitchCharacter();
+        }
+
+        if ( Input.GetKeyDown( KeyCode.A ) || Input.GetKeyDown( KeyCode.LeftArrow ) ) {
+            ChangeDirection( EDirection.Left );
+            SwitchCharacter();
+        }
+
+        if ( Input.GetKeyDown( KeyCode.D ) || Input.GetKeyDown( KeyCode.RightArrow ) ) {
+            ChangeDirection( EDirection.Right );
+            SwitchCharacter();
+        }
+
+        if ( Input.GetKeyUp( KeyCode.W ) || Input.GetKeyUp( KeyCode.UpArrow ) ||
+            Input.GetKeyUp( KeyCode.S ) || Input.GetKeyUp( KeyCode.DownArrow ) ||
+            Input.GetKeyUp( KeyCode.A ) || Input.GetKeyUp( KeyCode.LeftArrow ) ||
+            Input.GetKeyUp( KeyCode.D ) || Input.GetKeyUp( KeyCode.RightArrow ) ) {
+            //currentDirection = previousDirection;
+            //previousDirection = EDirection.None;
+
+            if ( !Input.anyKey ) {
+                currentDirection |= EDirection.None;
+                //ChangeDirection( EDirection.None );
+                //SwitchCharacter();
+            } else {
+                if ( Input.GetKey( KeyCode.W ) || Input.GetKey( KeyCode.UpArrow ) ) {
+                    ChangeDirection( EDirection.Up );
+                    SwitchCharacter();
+                }
+
+                if ( Input.GetKey( KeyCode.S ) || Input.GetKey( KeyCode.DownArrow ) ) {
+                    ChangeDirection( EDirection.Down );
+                    SwitchCharacter();
+                }
+
+                if ( Input.GetKey( KeyCode.A ) || Input.GetKey( KeyCode.LeftArrow ) ) {
+                    ChangeDirection( EDirection.Left );
+                    SwitchCharacter();
+                }
+
+                if ( Input.GetKey( KeyCode.D ) || Input.GetKey( KeyCode.RightArrow ) ) {
+                    ChangeDirection( EDirection.Right );
+                    SwitchCharacter();
+                }
+            }
+        }
+
+        var mov = new Vector3();
+
+        switch ( currentDirection ) {
+            case EDirection.Left:
+                mov.x = -Speed;
+                break;
+            case EDirection.Right:
+                mov.x = Speed;
+                break;
+            case EDirection.Up:
+                mov.y = Speed;
+                break;
+            case EDirection.Down:
+                mov.y = -Speed;
+                break;
+        }
+
+        if ( ( currentDirection & EDirection.None ) == EDirection.None ) {
+            mov.x = 0;
+            mov.y = 0;
+        }
+
+        transform.position += mov * Time.deltaTime;
+    }
+
+    private void ChangeDirection( EDirection newDirection ) {
+        //previousDirection = currentDirection;
+        currentDirection = newDirection;
+    }
+
+    private void SwitchCharacter() {
+        CDown.SetActive( currentDirection == EDirection.Down );
+        CUp.SetActive( currentDirection == EDirection.Up );
+        CLeft.SetActive( currentDirection == EDirection.Left );
+        CRight.SetActive( currentDirection == EDirection.Right );
+    }
 }
--- OnTheRun/Assets/Code/Tuna.cs
+++ OnTheRun/Assets/Code/Tuna.cs
@@ -1,15 +1,161 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
+using System;
 
+[ExecuteInEditMode]
 public class Tuna : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private int[,] grid = {
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+
+    [HideInInspector]
+    public GameObject Player;
+    public float Speed = 2.5f;
+    public bool DrawGrid = true;
+
+    private Rect gInfo = new Rect();
+
+    // Use this for initialization
+    void Start() {
+        if ( !Application.isPlaying ) return;
+        Player = GameObject.Find( "Player" );
+
+        var width = ( (float)Screen.width / (float)grid.GetLength( 1 ) );
+        var height = ( (float)Screen.height / (float)grid.GetLength( 0 ) );
+        var a = Camera.main.ScreenToWorldPoint( new Vector3( 0, 0 ) );
+        var b = Camera.main.ScreenToWorldPoint( new Vector3( width, height ) );
+
+        gInfo = new Rect( a.x, a.y, b.x - a.x, b.y - a.y );
+    }
+
+    void CalculateNew( float x, float y, ref float dist, ref Vector3 fpos ) {
+        if ( grid[8 - (int)y, (int)x] == 1 ) return;
+
+        var tpos = GetPosition( x, y );
+        var tdist = ( tpos - Player.transform.position ).magnitude;
+        if ( tdist > dist ) {
+            dist = tdist;
+            fpos = tpos;
+        }
+    }
+
+    private Vector3 delta = new Vector3();
+    private Vector3 newp = new Vector3();
+    private bool found = false;
+
+    // Update is called once per frame
+    void Update() {
+        if ( !Application.isPlaying ) return;
+
+        if ( !found ) {
+            found = true;
+            var coords = GetCoordinates();
+
+            var dist = 0f;
+            var fpos = new Vector3();
+            var tpos = new Vector3();
+
+            CalculateNew( coords.x, coords.y - 1, ref dist, ref fpos );
+            CalculateNew( coords.x, coords.y, ref dist, ref fpos );
+            CalculateNew( coords.x - 1, coords.y, ref dist, ref fpos );
+            CalculateNew( coords.x, coords.y, ref dist, ref fpos );
+            CalculateNew( coords.x + 1, coords.y, ref dist, ref fpos );
+            CalculateNew( coords.x, coords.y + 1, ref dist, ref fpos );
+
+            var trpos = transform.position;
+            trpos.x = (float)Math.Round( trpos.x, 1 );
+            trpos.y = (float)Math.Round( trpos.y, 1 );
+            fpos.x = (float)Math.Round( fpos.x, 1 );
+            fpos.y = (float)Math.Round( fpos.y, 1 );
+
+            delta = new Vector3();
+            if ( trpos.x < fpos.x ) {
+                delta.x = Speed;
+            } else if ( trpos.x > fpos.x ) {
+                delta.x = -Speed;
+            }
+
+            if ( trpos.y < fpos.y ) {
+                delta.y = Speed;
+            } else if ( trpos.y > fpos.y ) {
+                delta.y = -Speed;
+            }
+
+            newp = fpos;
+        }
+
+
+        transform.position += delta * Time.deltaTime;
+
+        var apos = transform.position;
+        apos.x = (float)Math.Round( apos.x, 1 );
+        apos.y = (float)Math.Round( apos.y, 1 );
+        newp.x = (float)Math.Round( newp.x, 1 );
+        newp.y = (float)Math.Round( newp.y, 1 );
+
+        if ( apos == newp ) {
+            found = false;
+        }
+    }
+
+    private Vector3 GetCoordinates() {
+        for ( int x = 0; x < 16; x++ ) {
+            for ( int y = 0; y < 9; y++ ) {
+                if ( grid[8 - y, x] == 1 ) continue;
+
+                var rect = new Rect( gInfo.x + ( gInfo.width * x ), gInfo.y + ( gInfo.height * y ), gInfo.width, gInfo.height );
+                if ( rect.Contains( transform.position ) ) {
+                    return new Vector3( x, y );
+                }
+            }
+        }
+
+        return new Vector3( -1, -1 );
+    }
+
+    private Rect GetRect( int x, int y ) {
+        return new Rect( gInfo.x + ( gInfo.width * x ), gInfo.y + ( gInfo.height * y ), gInfo.width, gInfo.height );
+    }
+
+    private Vector3 GetPosition( int x, int y ) {
+        return new Vector3(
+            gInfo.x + ( gInfo.width * x ) + ( gInfo.width / 2 ),
+            gInfo.y + ( gInfo.height * y ) + ( gInfo.height / 2 ) );
+    }
+
+    private Vector3 GetPosition( float x, float y ) {
+        return GetPosition( (int)x, (int)y );
+    }
+
+    public void OnGUI() {
+        if ( !DrawGrid ) return;
+
+        var width = ( (float)Screen.width / (float)grid.GetLength( 1 ) );
+        var height = ( (float)Screen.height / (float)grid.GetLength( 0 ) );
+        var xmax = Screen.width / width;
+        var ymax = Screen.height / height;
+
+        for ( int x = 0; x < xmax; x++ ) {
+            for ( int y = 0; y < ymax; y++ ) {
+                var val = grid[y, x];
+                if ( val == 0 ) {
+                    GUI.Box( new Rect( x * width, y * height, width, height ), "" );
+                } else {
+                    GUI.Box( new Rect( x * width, y * height, width, height ), "" );
+                    GUI.Box( new Rect( x * width, y * height, width, height ), "" );
+                    GUI.Box( new Rect( x * width, y * height, width, height ), "" );
+                }
+            }
+        }
+    }
 }
